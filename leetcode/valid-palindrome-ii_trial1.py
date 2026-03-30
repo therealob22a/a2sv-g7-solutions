0ea99219fe49431cebdc9bdf9873ec1aaeef0ec8
Obssa Degefu <obssa.ugr-1131-16@aau.edu.et>
2026-03-30 15:09:41 +0300

    Add solution for Valid Palindrome II (trial 1)

---
+++ leetcode/valid-palindrome-ii_trial1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        n = len(s)
+        left, right = 0, n-1
+
+        while left<right:
+            if s[left]!=s[right]:
+                skipLeft = s[left+1:right+1]
+                skipRight = s[left:right]
+
+                return skipLeft==skipLeft[::-1] or skipRight==skipRight[::-1]
+
+            left+=1
+            right-=1
+
+        return True

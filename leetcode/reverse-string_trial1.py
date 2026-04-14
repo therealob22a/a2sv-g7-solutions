12ffa022ee19b4bc1aa853bc1f88bf19a4e2d6ef
Obssa Degefu <obssa.ugr-1131-16@aau.edu.et>
2026-04-14 20:49:21 +0300

    Add solution for Reverse String (trial 1)

--- leetcode/reverse-string_trial1.py
+++ leetcode/reverse-string_trial1.py
@@ -3,11 +3,14 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        left = 0
-        right = len(s)-1
+        n=len(s)
+
+        left=0
+        right=n-1
 
         while left<right:
             s[left],s[right]=s[right],s[left]
             left+=1
             right-=1
         
+        return s
